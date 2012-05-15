d53b09b53588e4db06343a4669d916e443582914
rankida <david.rankin@spartan-solutions.com>
2012-05-15 22:11:40 +0100

    initial commit
    Setup VS2008 solution with Jabber.Net and a WPF application.
    Not yet working and very crude, but on it's way.

---
+++ WpfApplication1/WpfApplication1/JabberLib.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WpfApplication1
+{
+    using System.Xml;
+
+    using bedrock;
+
+    using jabber;
+    using jabber.client;
+    using jabber.connection;
+    using jabber.protocol.client;
+    using jabber.protocol.iq;
+
+    public class JabberLib
+    {
+        private string username;
+
+        private string password;
+
+        private string host;
+
+        private int port = 5222;
+
+        private JabberClient jabberClient;
+
+        private CapsManager capabilityManager;
+
+        public JabberLib(string username, string password, string host)
+        {
+            this.username = username;
+            this.password = password;
+            this.host = host;
+        }
+
+        /// <summary>
+        /// Gets or sets the action to be performed when text is read
+        /// in.
+        /// </summary>
+        public Action<string> OnReadText { get; set; }
+
+        /// <summary>
+        /// Gets or sets the action perfomred when things go wrong
+        /// </summary>
+        public Action<object, Exception> OnError { get; set; }
+
+        /// <summary>
+        /// Gets or sets the action called when the stream has a tizzy
+        /// </summary>
+        public Action<object, XmlElement> OnStreamError { get; set; }
+
+        public void Connect(bool initialPresence)
+        {
+            this.jabberClient = new JabberClient();
+            this.jabberClient.OnReadText += this.InternalOnReadText;
+            this.jabberClient.OnError += new ExceptionHandler(this.OnError);
+            this.jabberClient.OnStreamError += new jabber.protocol.ProtocolHandler(this.OnStreamError);
+
+            // regisert presence
+            this.jabberClient.AutoLogin = false;
+            this.jabberClient.OnLoginRequired += this.jc_OnLoginRequired;
+            this.jabberClient.OnRegisterInfo += this.jc_OnRegisterInfo;
+            this.jabberClient.OnRegistered += this.jc_OnRegistered;
+
+            var jid = new JID(this.username);
+            this.jabberClient.User = jid.User;
+            this.jabberClient.Server = jid.Server;
+            this.jabberClient.NetworkHost = this.host;
+            this.jabberClient.Port = this.port;
+            this.jabberClient.Resource = "Hangout Phone";
+            this.jabberClient.Password = this.password;
+            this.jabberClient.AutoStartTLS = true;
+            this.jabberClient.AutoPresence = initialPresence;
+            this.jabberClient.AutoReconnect = 3f;
+
+            this.capabilityManager = new CapsManager { Stream = this.jabberClient, Node = "http://rankida.com" };
+
+            this.jabberClient.Connect();
+        }
+
+        private void InternalOnReadText(object sender, string txt)
+        {
+            if (string.IsNullOrEmpty(txt))
+            {
+                return;
+            }
+
+            Console.Out.WriteLine("RECV: " + txt);
+            this.OnReadText(txt);
+        }
+
+        public void Write(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Close()
+        {
+            this.jabberClient.Close();
+            this.jabberClient.Dispose();
+            this.jabberClient = null;
+        }
+
+        private void jc_OnLoginRequired(object sender)
+        {
+            var jc = (JabberClient)sender;
+            jc.Register(new JID(jc.User, jc.Server, null));
+        }
+
+        private void jc_OnRegistered(object sender, IQ iq)
+        {
+            var jc = (JabberClient)sender;
+            if (iq.Type == IQType.result)
+            {
+                jc.Login();
+            }
+        }
+
+        private bool jc_OnRegisterInfo(object sender, Register r)
+        {
+            return true;
+        }
+    }
+}
---
+++ WpfApplication1/WpfApplication1/Window1.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    using System.Xml;
+
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        private JabberLib jabber;
+
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void connect_Click(object sender, RoutedEventArgs e)
+        {
+            var username = this.usernameBox.Text;
+            var password = this.passwordBox.Password;
+            var host = "talk.google.com";
+
+            this.jabber = new JabberLib(username, password, host)
+                {
+                    OnReadText = this.Display, OnError = this.OnError, OnStreamError = this.OnStreamError 
+                };
+            this.jabber.Connect(true);
+            this.messageBox.IsEnabled = true;
+            this.send.IsEnabled = true;
+        }
+
+        private void Display(string raw)
+        {
+            this.chatDisplay.AppendText(raw);
+        }
+
+        private void OnError(object arg1, Exception ex)
+        {
+            throw new NotImplementedException("Error: " + ex.Message);
+        }
+
+        private void OnStreamError(object arg1, XmlElement arg2)
+        {
+            throw new NotImplementedException("Stream Error");
+        }
+    }
+}
